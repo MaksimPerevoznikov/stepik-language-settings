39750c2297f26cb3efacb17695c13c8597ebf3fc
MaksimPerevoznikov <51896853+MaksimPerevoznikov@users.noreply.github.com>
2019-07-22 14:45:11 +0700

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,7 +3,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_guest_should_see_login_link(browser):
+def test_button_display_for_different_languages(browser):
     browser.get(link)
     time.sleep(30)
     button = browser.find_element_by_css_selector('.btn.btn-lg.btn-primary.btn-add-to-basket')
