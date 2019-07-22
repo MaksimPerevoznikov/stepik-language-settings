576c5cf8df37f2e0f4eaca5588b292f30ab73689
MaksimPerevoznikov <51896853+MaksimPerevoznikov@users.noreply.github.com>
2019-07-22 12:57:21 +0700

    Update test_items.py
    Add time.sleep(30)

--- test_items.py
+++ test_items.py
@@ -5,6 +5,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_login_link(browser):
     browser.get(link)
-    time.sleep(3)
+    time.sleep(30)
     browser.find_element_by_css_selector('.btn.btn-lg.btn-primary.btn-add-to-basket')
 
