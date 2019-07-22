ca42becbe071cab398862bb0b66b5c0e4f282199
MaksimPerevoznikov <51896853+MaksimPerevoznikov@users.noreply.github.com>
2019-07-22 14:23:04 +0700

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -6,5 +6,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_guest_should_see_login_link(browser):
     browser.get(link)
     time.sleep(30)
-    browser.find_element_by_css_selector('.btn.btn-lg.btn-primary.btn-add-to-basket')
-
+    button = browser.find_element_by_css_selector('.btn.btn-lg.btn-primary.btn-add-to-basket')
+    assert button is not None
